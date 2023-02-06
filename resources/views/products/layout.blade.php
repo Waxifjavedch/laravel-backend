da61a8ac93fa256f4caf2bf76baa82600aee156c
Waxifjavedch <57950097+Waxifjavedch@users.noreply.github.com>
2023-02-06 21:28:52 +0500

    Add files via upload

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Backed Laravel Of Our Project</title>
+    <link href="https://bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+  
+<div class="container">
+    @yield('content')
+</div>
+   
+</body>
+</html>
---
+++ storage/framework/views/d584bf66679e3792bfc237df2624953a2db4b468.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 8 CRUD Application - ItSolutionStuff.com</title>
+    <link href="https://bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+  
+<div class="container">
+    <?php echo $__env->yieldContent('content'); ?>
+</div>
+   
+</body>
+</html><?php /**PATH /Users/chwasifjaat/Desktop/projects/example-app/resources/views/products/layout.blade.php ENDPATH**/ ?>
